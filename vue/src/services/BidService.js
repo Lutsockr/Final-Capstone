72622a7a1eee1ed6a48be5a7e5e62b78ae66b8e6
Ben Oakland <106042763+pojOakland@users.noreply.github.com>
2022-12-12 10:15:02 -0500

    created bid service

---
+++ vue/src/services/BidService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: "http://localhost:9000"
+});
+
+export default {
+    placeBid(bid) {
+        return http.post('/bids', bid)
+    },
+
+    getBidById(bidId) {
+        return http.get(`/bids/${bidId}`)
+    },
+
+    getBidsByUserId(userId) {
+        return http.get(`/bids/user/{userId}`)
+    }
+
+}
