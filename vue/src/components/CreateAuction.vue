6489f08d5f9d941d08d6b2e8ffec248c4ee0882f
Timothy OConnor <copper22tone@gmail.com>
2022-12-12 09:57:50 -0500

    style create

--- vue/src/components/CreateAuction.vue
+++ vue/src/components/CreateAuction.vue
@@ -1,27 +1,27 @@
 <template>
   <form v-on:submit.prevent>
     <div class="field">
-      <label for="title">Title</label>
+      <label for="title">Title </label>
       <input type="text" name="title" v-model="auction.title" />
     </div>
     <div class="field">
-      <label for="description">Description</label>
+      <label for="description">Description </label>
       <input type="text" name="description" v-model="auction.description" /> 
     </div>
     <div class="field">
-      <label for="startingPrice">Starting Price</label>
+      <label for="startingPrice">Starting Price </label>
       <input type="text" name="startingPrice" v-model="auction.startingPrice" />
     </div>
     <div class="field">
-      <label for="auctionType">Auction Type</label>
+      <label for="auctionType">Auction Type </label>
       <input type="text" name="typeId" v-model="auction.auctionType.id" />
     </div>
     <div class="field">
-      <label for="endDate">End Date</label>
+      <label for="endDate">End Date </label>
       <input type="datetime-local" name="endDate" v-model="auction.endDate" />
     </div>
     <div class="field">
-      <label for="imagePath">Image Url</label>
+      <label for="imagePath">Image Url </label>
       <input type="url" name="imagePath" v-model="auction.imagePath" />
     </div>
     <div class="actions">
@@ -60,3 +60,15 @@ export default {
   }
 };
 </script>
+<style>
+div.field{
+  padding: 10px;
+}
+form{
+  justify-content: center;
+  border-color: cornflowerblue;
+  border-radius: 10px;
+  border-style: solid;
+  background-color: cornflowerblue;
+}
+</style>
