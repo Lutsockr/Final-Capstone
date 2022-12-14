55cd35cd86d4c8a47af4160c4a808e744c8f743f
Christian Breitmos <baba4172@gmail.com>
2022-12-14 14:42:37 -0500

    made a User Profile view where they can see their information

---
+++ vue/src/components/Profile.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="profile-main">
+        <h1 class="profile" style="color:white"> Profile </h1>
+         <div class="contact-info">
+         <h1 style="color:white"> {{ this.$store.state.user.username }}'s Profile </h1>
+         <h2 >Contact Info<br>Username: {{this.$store.state.user.username}}<br>Name:<br>Address:<br>Phone Number:</h2>
+     
+         </div>
+        
+    </div>
+</template>
+<script>
+export default {
+name: 'profile'
+  
+}
+</script>
+<style scoped>
+
+div
+{
+    /* width: 600px;
+    height: 100px;
+    background: pink;
+    cursor: url(https://cdn.custom-cursor.com/packs/3743/neon-money-pack.png) 25 15, auto; */
+    
+    color:white;
+}
+.profile{
+color: white
+}
+h2{
+    float:left
+}
+
+</style>
