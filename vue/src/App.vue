feec1ed28abb9c2773f05b1c6b1bd098c9e87e61
Christian Breitmos <baba4172@gmail.com>
2022-12-15 09:54:22 -0500

    Add title to the document.title and fix the cursor so it is centered now

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -109,7 +109,7 @@ html {
 background-image: radial-gradient(circle at center center, #fb00ff, #639f7e), repeating-radial-gradient(circle at center center, #fb00ff, #2908ff, 91px, transparent 182px, transparent 91px);
 background-blend-mode: multiply;
 background-color: #639f7e;
-  cursor:url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>💰</text></svg>") 16 0,auto; /*!emojicursor.app*/
+  cursor:url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>💰</text></svg>") 25 15,auto; /*!emojicursor.app*/
 }
 div.topnav {
   animation: colorChange 20s;
